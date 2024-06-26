00cdb2afc1309901ea97e0499b16086bf7782243
CrissRivas <cristopher_rivas@outlook.es>
2024-06-26 16:35:32 -0600

    Created config

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { json } from 'express';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
+
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.use(json({ limit: '60mb' }));
+
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Ayudate')
+    .setDescription('API de Ayudate')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(PORT);
+}
+bootstrap();
