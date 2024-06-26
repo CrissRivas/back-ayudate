f2d23eaaf59fde425707fa2b9d15ce489aa350e1
CrissRivas <cristopher_rivas@outlook.es>
2024-06-26 17:56:52 -0600

    Add(DTO): Add DTO update-product

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
+import { IsInt, IsPositive } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @ApiProperty({ example: 1 })
+  @IsInt()
+  @IsPositive()
+  id: number;
+}
